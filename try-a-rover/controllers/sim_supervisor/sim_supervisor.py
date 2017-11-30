906ffb03284e4eaee3a39c7379295c20c225abb5
Enric Cervera <ecervera@uji.es>
2017-11-30 19:35:27 +0100

    supervisor

--- try-a-rover/controllers/sim_supervisor/sim_supervisor.py
+++ try-a-rover/controllers/sim_supervisor/sim_supervisor.py
@@ -3,11 +3,14 @@ from controller import Supervisor
 import rospy
 from std_srvs.srv import Empty
 
+s = Supervisor()
+
 def handle_restart_sim(req):
-	s = Supervisor()
 	s.simulationRevert()
 
 rospy.init_node('sim_supervisor')
-s = rospy.Service('restart_sim', Empty, handle_restart_sim)
-rospy.spin()
+rospy.Service('restart_sim', Empty, handle_restart_sim)
+
+while s.step(32)!=-1:
+	pass
 
