2965c0a44c217b181a54adfc5ac932dac2c9d872
Enric Cervera <ecervera@uji.es>
2017-12-04 11:21:13 +0100

    added load world functionality

--- try-a-pioneer/controllers/sim_supervisor/sim_supervisor.py
+++ try-a-pioneer/controllers/sim_supervisor/sim_supervisor.py
@@ -1,17 +1,22 @@
 from controller import Supervisor
 
 import rospy
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, String
 
 class SimSupervisor (Supervisor) :
 
   def restart_callback(self,msg):
     self.simulationRevert()
 
+  def load_world_callback(self,msg):
+    print('World file: %s' % msg.data)
+    self.loadWorld(msg.data)
+
   def run(self):
     rospy.init_node('sim_supervisor', anonymous=True)
     rospy.sleep(3.0)
     rospy.Subscriber('restart_sim', Empty, self.restart_callback)
+    rospy.Subscriber('load_world', String, self.load_world_callback)
 
     timeStep = int(self.getBasicTimeStep())
     rate = 1000 / float(timeStep)
