717d521940e91e46429fac89207a2d3b4d576f48
Enric Cervera <ecervera@uji.es>
2020-03-31 10:58:24 +0200

    working rpyc

--- RobotServer.py
+++ RobotServer.py
@@ -17,5 +17,5 @@ class RobotService(rpyc.Service):
     
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(RobotService(), port=18861)
+    t = ThreadedServer(RobotService(), port=18861, protocol_config={'allow_public_attrs': True,})
     t.start()
